d1baccb96a974a12c29467b72afd2dbc4bce590a
James Matson <James.Matson@api.net.au>
2025-08-18 21:31:03 +1000

    Add DynamoDB local support and account management
    Integrate Amazon DynamoDB SDK for local development.
    Configure AWS Lambda to wait for DynamoDB local resource.
    Create "Accounts" table with primary key and billing mode.
    Add sample account data upon resource readiness.
    Ensure application builds and runs with new functionality.

--- AspireLambda.AppHost/AppHost.cs
+++ AspireLambda.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda;
 using Aspire.Hosting;
 using Aspire.Hosting.AWS.Lambda;
@@ -15,9 +17,45 @@ var addFunction = builder.AddAWSLambdaFunction<Projects.AspireLambda>("AddFuncti
         LogFormat = LogFormat.JSON,
     })
     .WithReference(cache, "cache")
-    .WaitFor(cache);
+    .WaitFor(cache)
+    .WithReference(dynamoDbLocal)
+    .WaitFor(dynamoDbLocal);
 
 builder.AddAWSAPIGatewayEmulator("APIGatewayEmulator", Aspire.Hosting.AWS.Lambda.APIGatewayType.HttpV2)
     .WithReference(addFunction, Aspire.Hosting.AWS.Lambda.Method.Get, "/add/{x}/{y}");
+
+builder.Eventing.Subscribe<ResourceReadyEvent>(dynamoDbLocal.Resource, async (evnt, ct) =>
+{
+    // Configure DynamoDB service client to connect to DynamoDB local.
+    var serviceUrl = dynamoDbLocal.Resource.GetEndpoint("http").Url;
+    var ddbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig { ServiceURL = serviceUrl });
+
+    // Create the Accounts table.
+    var creatResponse = await ddbClient.CreateTableAsync(new CreateTableRequest
+    {
+        TableName = "Accounts",
+        AttributeDefinitions = new List<AttributeDefinition>
+        {
+            new AttributeDefinition { AttributeName = "Id", AttributeType = "S" }
+        },
+        KeySchema = new List<KeySchemaElement>
+        {
+            new KeySchemaElement { AttributeName = "Id", KeyType = "HASH" }
+        },
+        BillingMode = BillingMode.PAY_PER_REQUEST
+    });
+
+    // Add an account to the Accounts table.
+    await ddbClient.PutItemAsync(new PutItemRequest
+    {
+        TableName = "Accounts",
+        Item = new Dictionary<string, AttributeValue>
+        {
+            { "Id", new AttributeValue("1") },
+            { "Name", new AttributeValue("Amazon") },
+            { "Address", new AttributeValue("Seattle, WA") }
+        }
+    });
+});
 
 builder.Build().Run();
